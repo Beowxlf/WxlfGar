4bb7f933f62da5f5011e9cfd2c09a1b04464c6f0
Beowxlf <110790619+Beowxlf@users.noreply.github.com>
2026-03-02 23:22:49 -0600

    feat: implement phase-1 baseline workflow and detection logic

---
+++ internal/modules/bundle/module.go
@@ -0,0 +1,74 @@
+package bundle
+
+import (
+	"archive/zip"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+type PrepareInput struct{ OutputRoot string }
+
+type Module interface {
+	PrepareLayout(context.Context, PrepareInput) (string, error)
+	Compress(context.Context, string) (string, error)
+}
+
+type Default struct{}
+
+func NewDefault() *Default { return &Default{} }
+
+func (n *Default) PrepareLayout(_ context.Context, in PrepareInput) (string, error) {
+	hostname, _ := os.Hostname()
+	if hostname == "" {
+		hostname = "HOST"
+	}
+	hostname = strings.ReplaceAll(hostname, " ", "_")
+	ts := time.Now().UTC().Format("20060102_150405")
+	bundleName := fmt.Sprintf("Wulfgar_%s_%s", hostname, ts)
+	bundlePath := filepath.Join(in.OutputRoot, bundleName)
+	for _, dir := range []string{"", "triage", "slices"} {
+		if err := os.MkdirAll(filepath.Join(bundlePath, dir), 0o755); err != nil {
+			return "", err
+		}
+	}
+	return bundlePath, nil
+}
+
+func (n *Default) Compress(_ context.Context, bundlePath string) (string, error) {
+	zipPath := bundlePath + ".zip"
+	out, err := os.Create(zipPath)
+	if err != nil {
+		return "", err
+	}
+	defer out.Close()
+
+	zw := zip.NewWriter(out)
+	defer zw.Close()
+
+	err = filepath.Walk(bundlePath, func(path string, info os.FileInfo, walkErr error) error {
+		if walkErr != nil || info.IsDir() {
+			return walkErr
+		}
+		rel := strings.TrimPrefix(path, bundlePath+string(os.PathSeparator))
+		w, err := zw.Create(rel)
+		if err != nil {
+			return err
+		}
+		in, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer in.Close()
+		_, err = io.Copy(w, in)
+		return err
+	})
+	if err != nil {
+		return "", err
+	}
+	return zipPath, nil
+}
---
+++ internal/modules/report/module.go
@@ -0,0 +1,60 @@
+package report
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/wxlfgar/wulfgar/internal/contracts"
+)
+
+type Input struct {
+	BundlePath string
+	Machine    contracts.MachineReport
+}
+
+type Output struct{ Files []string }
+
+type Module interface {
+	Generate(context.Context, Input) (Output, error)
+}
+
+type Default struct{}
+
+func NewDefault() *Default { return &Default{} }
+
+func (n *Default) Generate(_ context.Context, in Input) (Output, error) {
+	machinePath := filepath.Join(in.BundlePath, "machine.json")
+	summaryPath := filepath.Join(in.BundlePath, "summary.txt")
+
+	blob, err := json.MarshalIndent(in.Machine, "", "  ")
+	if err != nil {
+		return Output{}, err
+	}
+	if err := os.WriteFile(machinePath, blob, 0o644); err != nil {
+		return Output{}, err
+	}
+
+	summary := []string{
+		"Wulfgar Summary",
+		fmt.Sprintf("GeneratedUTC: %s", time.Now().UTC().Format(time.RFC3339)),
+		fmt.Sprintf("Host: %s", in.Machine.Host.Hostname),
+		fmt.Sprintf("Interface: %s", in.Machine.Capture.Interface),
+		fmt.Sprintf("CaptureDurationSeconds: %d", in.Machine.Capture.DurationSeconds),
+		fmt.Sprintf("EventsDetected: %d", len(in.Machine.Events)),
+		fmt.Sprintf("DNS NXDOMAIN: %d", in.Machine.Metrics.DNS.NXDOMAINCount),
+		fmt.Sprintf("DNS SERVFAIL: %d", in.Machine.Metrics.DNS.SERVFAILCount),
+		fmt.Sprintf("TCP SYN Retransmits: %d", in.Machine.Metrics.TCP.SYNRetransmits),
+		fmt.Sprintf("TCP Resets: %d", in.Machine.Metrics.TCP.ConnectionReset),
+		fmt.Sprintf("ICMP Unreachable: %d", in.Machine.Metrics.ICMP.DestinationUnreachable),
+		fmt.Sprintf("ICMP TTL Exceeded: %d", in.Machine.Metrics.ICMP.TTLExceeded),
+	}
+	if err := os.WriteFile(summaryPath, []byte(strings.Join(summary, "\n")+"\n"), 0o644); err != nil {
+		return Output{}, err
+	}
+	return Output{Files: []string{machinePath, summaryPath}}, nil
+}
